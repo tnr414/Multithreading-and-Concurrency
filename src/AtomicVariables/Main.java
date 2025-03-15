07c2e7d52bd190a0e25b8faf3c5c32063fac7b07
Tanveer Azam Ansari <tanveerazamansari@MacBookAir.lan>
2025-03-15 12:30:38 +0530

    Atomic variable example

--- src/AtomicVariables/Main.java
+++ src/AtomicVariables/Main.java
@@ -1,4 +1,31 @@
 package AtomicVariables;
 
 public class Main {
+    public static void main(String[] args) {
+        SharedResource sharedResource = new SharedResource();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 200; i++) {
+                sharedResource.increment();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 200; i++) {
+                sharedResource.increment();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();;
+        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+        }
+
+        System.out.println(sharedResource.get());
+    }
 }
--- src/AtomicVariables/SharedResource.java
+++ src/AtomicVariables/SharedResource.java
@@ -1,4 +1,15 @@
 package AtomicVariables;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class SharedResource {
+    AtomicInteger counter = new AtomicInteger(0);
+
+    public void increment() {
+        counter.incrementAndGet();
+    }
+
+    public int get() {
+        return counter.get();
+    }
 }
